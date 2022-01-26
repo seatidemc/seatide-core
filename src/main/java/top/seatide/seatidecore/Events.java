87c808a3d00bdcf30f8818937a634b630cc79505
Subilan <suyuanliang1995@gmail.com>
2022-01-26 13:41:09 +0800

    fix `@EventHandler`

--- src/main/java/top/seatide/seatidecore/Events.java
+++ src/main/java/top/seatide/seatidecore/Events.java
@@ -1,10 +1,12 @@
 package top.seatide.seatidecore;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class Events implements Listener {
+    @EventHandler
     public void banExplosion(EntityExplodeEvent e) {
         if (e.getEntityType() == EntityType.PRIMED_TNT || e.getEntityType() == EntityType.CREEPER) {
             e.blockList().clear();
